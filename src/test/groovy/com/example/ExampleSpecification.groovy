9358896ae7ca871acd55711ac394f6471301333d
Umadevi-R <umad18323.cs@rmkec.ac.in>
2022-07-23 23:26:16 +0530

    Wrote a simple Assertion

---
+++ src/test/groovy/com/example/ExampleSpecification.groovy
@@ -0,0 +1,10 @@
+package com.example
+
+import spock.lang.Specification
+
+class ExampleSpecification extends Specification{
+    def "should be a simple assertion"() {
+        expect:
+            1 == 1
+    }
+}
